e588da4efa098445f6d9fa61be48badd919cd3dc
xbraham <46814866+xbraham@users.noreply.github.com>
2022-07-31 16:36:46 +0100

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("***********");
+        System.out.println("* Welcome *");
+        System.out.println("***********");
+    }
+}
